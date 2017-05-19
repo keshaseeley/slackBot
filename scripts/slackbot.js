adfa3ab813336f09cf31d504fd4e91575fab507f
Kesha Seeley <keshalynne@gmail.com>
2017-05-19 11:29:56 -0700

    stones

--- scripts/slackbot.js
+++ scripts/slackbot.js
@@ -39,21 +39,21 @@ If you want to supplement your existing code above with any the code below,
 you need to move the contents of module.exports below into the module.exports code above. 
 */
 
-var squirrels;
-squirrels = ["http://img.skitch.com/20100714-d6q52xajfh4cimxr3888yb77ru.jpg", "https://img.skitch.com/20111026-r2wsngtu4jftwxmsytdke6arwd.png", "http://cl.ly/1i0s1r3t2s2G3P1N3t3M/Screen_Shot_2011-10-27_at_9.36.45_AM.png", "http://shipitsquirrel.github.com/images/squirrel.png"];
+// var squirrels;
+// squirrels = ["http://img.skitch.com/20100714-d6q52xajfh4cimxr3888yb77ru.jpg", "https://img.skitch.com/20111026-r2wsngtu4jftwxmsytdke6arwd.png", "http://cl.ly/1i0s1r3t2s2G3P1N3t3M/Screen_Shot_2011-10-27_at_9.36.45_AM.png", "http://shipitsquirrel.github.com/images/squirrel.png"];
 
-module.exports = function(robot) {
-  // Basic example of respond / send. If the user enters hi or hello the bot responds "Howdy!" 
-  return robot.respond(/hi|hello/i, function(msg) {
-    return msg.send("Howdy!");
-  });
+// module.exports = function(robot) {
+//   // Basic example of respond / send. If the user enters hi or hello the bot responds "Howdy!" 
+//   return robot.respond(/hi|hello/i, function(msg) {
+//     return msg.send("Howdy!");
+//   });
 
-  // Random Example
-  //If a user enters 'ship it' we return a random squirrel, which is popular for symbolizing shipping something with engineers
-  return robot.hear(/ship it/i, function(msg) {
-    return msg.send(msg.random(squirrels));
-  });
-};
+//   // Random Example
+//   //If a user enters 'ship it' we return a random squirrel, which is popular for symbolizing shipping something with engineers
+//   return robot.hear(/ship it/i, function(msg) {
+//     return msg.send(msg.random(squirrels));
+//   });
+// };
 
 
 /* Pseudocode
@@ -75,12 +75,12 @@ if no, respond msg: simma down now
 
 */
 
-// var stones;
-// stones = [
-//   "https://www.youtube.com/watch?v=8kl6q_9qZOs&index=17&list=PL11CC59281C5FDFB3",
-//   "https://www.youtube.com/watch?v=jo34VhfcetU&index=29&list=PL11CC59281C5FDFB3",
-//   "https://www.youtube.com/watch?v=hic-dnps6MU&list=PL11CC59281C5FDFB3&index=2"
-// ];
+var stones;
+stones = [
+  "https://www.youtube.com/watch?v=8kl6q_9qZOs&index=17&list=PL11CC59281C5FDFB3",
+  "https://www.youtube.com/watch?v=jo34VhfcetU&index=29&list=PL11CC59281C5FDFB3",
+  "https://www.youtube.com/watch?v=hic-dnps6MU&list=PL11CC59281C5FDFB3&index=2"
+];
 
 // var zeppelin;
 // zepplin = [
@@ -100,13 +100,13 @@ if no, respond msg: simma down now
 
 // var rock = stones + zeppelin + petty;
 
-// module.exports = function(robot) {
+module.exports = function(robot) {
 
-//    robot.hear(/gimme stones/i, function(msg) {
-//     return msg.send("Rock on!");
-//    });
+   robot.respond(/gimme stones/i, function(msg) {
+    return msg.send("Rock on!");
+   });
    
-//    return robot.hear(/Rock on/i, function(msg) {
-//     return msg.send(msg.random(stones));
-//   });
-// }
+   return robot.hear(/Rock on/i, function(msg) {
+    return msg.send(msg.random(stones));
+  });
+}
